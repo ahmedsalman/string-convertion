70bf9596626ada8466428481d2fdf9e692ebbceb
Ahmed Salman <ahmed.salman.noor@gmail.com>
2015-07-24 03:04:40 +0400

    heroku settings

--- string_manipulation/wsgi.py
+++ string_manipulation/wsgi.py
@@ -13,11 +13,7 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "string_manipulation.settings")
-
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
